07030193817f310cd76eee0a27c27a3ef04509c4
Abdullah Al Faroque <rfnshare@gmail.com>
2025-04-17 22:27:35 +0600

    added port

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request
 import pandas as pd
 import re
@@ -145,4 +147,5 @@ def dashboard():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
